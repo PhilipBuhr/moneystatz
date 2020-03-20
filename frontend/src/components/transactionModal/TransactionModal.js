dbdeeee4cdf08610c1052013c1cf5a265b9b5a42
Philip Buhr <philip.buhr@outlook.de>
2020-03-20 20:06:22 +0100

    show transaction data in modal

--- frontend/src/components/transactionModal/TransactionModal.js
+++ frontend/src/components/transactionModal/TransactionModal.js
@@ -4,11 +4,25 @@ import './TransactionModal.css';
 import {close} from "../../actions/transactionActions"
 
 class TransactionModal extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {transaction: this.props.transaction}
+    }
+
     render() {
         return (
             <div className={this.props.active ? "TransactionModal-container" : "hidden"}>
                 <div className="TransactionModal-modal">
-                    <button onClick={() => this.props.close()}>Close</button>
+                    <div className="TransactionModal-body">
+                        <div className="TransactionModal-label">Amount</div><input value={this.state.transaction.amount}/>
+                        <div className="TransactionModal-label">Jar</div><input value={this.state.transaction.jar}/>
+                        <div className="TransactionModal-label">Date</div><input value={this.state.transaction.date}/>
+                    </div>
+                    <div className="TransactionModal-button-box">
+                        <button className="TransactionModal-button submit" onClick={() => this.props.close()}>Submit</button>
+                        <button className="TransactionModal-button close" onClick={() => this.props.close()}>Close</button>
+                    </div>
                 </div>
             </div>
         );
@@ -16,7 +30,8 @@ class TransactionModal extends React.Component {
 }
 
 const mapStateToProps = state => ({
-    active: !!state.dateState.selectedTransaction
+    active: !!state.dateState.selectedTransaction,
+    transaction: state.dateState.selectedTransaction
 });
 
 const mapDispatchToProps = dispatch => ({
