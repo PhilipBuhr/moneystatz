a27c8f0330cb2d84998782fd3b055e7687a61c7a
Philip Buhr <philip.buhr@outlook.de>
2020-04-05 12:58:42 +0200

    fix database name

--- moneyStatz/settings.py
+++ moneyStatz/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'moneyStatz.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'monezstatz.db'),
+        'NAME': os.path.join(BASE_DIR, 'moneystatz.db'),
     }
 }
 
